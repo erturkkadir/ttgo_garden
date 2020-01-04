f357049e30185e5a99760455921a063c2e6426ba
Kadir <kadirerturk@gmail.com>
2020-01-03 17:41:20 -0800

    Gas sensors

--- gas_detect/gas_detect.ino
+++ gas_detect/gas_detect.ino
@@ -3,6 +3,12 @@
 #include <SPI.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <Wire.h>
+#include "SparkFun_SGP30_Arduino_Library.h" 
+
+SGP30 mySensor;
+
+#define I2C_SGP30  0x58
 /*TFT_WHITE TFT_BLACK TFT_YELLOW TFT_RED TFT_GREEN TFT_BLUE*/
 
 TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
@@ -57,7 +63,7 @@ void connectToWiFi() {
       tft.println("Trying to connect..."); 
     }
   }
-  tft.println(WiFi.localIP());
+  //tft.println(WiFi.localIP());
 }
 
 void setup() {
@@ -68,14 +74,39 @@ void setup() {
   tft.setCursor(1, 1, 4); // font 4
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+
+  Wire.begin();
+  Wire.setClock(400000);
+  if (mySensor.begin() == false) {
+    Serial.println("No SGP30 Detected. Check connections.");
+    while (1);
+  }
+  mySensor.initAirQuality();
+  connectToWiFi();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  tft.println("www.syshuman.com");
-  connectToWiFi();
-  client.publish("esp32/temperature", "1");
-  client.publish("esp32/humidity", "0");
+  //tft.println("www.syshuman.com");
   
-  while(1) yield(); // We must yield() to stop a watchdog timeout.
+  tft.setCursor(1, 20, 4); // font 4
+  mySensor.measureAirQuality();
+  tft.println();
+  tft.print("CO2: ");
+  tft.print(mySensor.CO2);
+  tft.print("  TVOC :");
+  tft.print(mySensor.TVOC);
+  mySensor.measureRawSignals();
+  tft.println();
+  tft.print("H2:");
+  tft.print(mySensor.H2);
+  tft.print(" Eth:");
+  tft.print(mySensor.ethanol);
+ 
+  //client.publish("esp32/temperature", "1");
+  //client.publish("esp32/humidity", "0");
+ 
+  yield(); // We must yield() to stop a watchdog timeout.
+  delay(1000);
+  
 }
